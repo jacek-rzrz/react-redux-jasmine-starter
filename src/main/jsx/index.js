c6317e65de8bd7f904b851a46750c0b57ce81b2e
Jacek Rzeniewicz <jacek@rzrz.pl>
2016-08-11 23:00:52 +0100

    Add LESS compilation

--- src/main/jsx/index.js
+++ src/main/jsx/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './counter/Counter';
+import Counter from 'counter/Counter';
+import "main.less";
 
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var config = {
-  context: path.join(__dirname, 'src', 'main', 'jsx'),
+  context: path.join(__dirname, 'src', 'main'),
   entry: [
-    './index.js',
+    './jsx/index.js'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
   },
   plugins: [
+    new ExtractTextPlugin('style.css'),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'main', 'resources', 'templates', 'index.html'),
       inject: 'head',
       hash: true
     })
   ],
+  devtool: 'source-map',
   module: {
     loaders: [
       {
@@ -23,11 +26,20 @@ var config = {
         exclude: /node_modules/,
         loaders: ['babel'],
       },
+      {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract(
+                    // activate source maps via loader query
+                    'css?sourceMap!' +
+                    'less?sourceMap'
+                )
+            }
     ],
   },
   resolve: {
     root: [
-      path.resolve(path.join(__dirname, 'src', 'main', 'jsx'))
+      path.resolve(path.join(__dirname, 'src', 'main', 'jsx')),
+      path.resolve(path.join(__dirname, 'src', 'main', 'resources', 'less'))
     ],
   },
 };
